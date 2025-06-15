0658a612c02c1220a3304266fdf22e713dd19b1d
PC <komarovitstudies@gmail.com>
2025-06-15 20:35:00 +0300

    Fix

--- src/middlewares/owner-validation.middleware.ts
+++ src/middlewares/owner-validation.middleware.ts
@@ -3,8 +3,10 @@ const userService = require('../services/user.service');
 
 export function checkOwner(req: Request, res: Response, next: NextFunction) {
 
-
-    const authenticatedUserId = req.user?.id; 
+    // This is a pragmatic cast to unblock a stubborn build-environment issue.
+    // The type `Request` should be globally augmented to include `user`,
+    // but the build environment is not picking up the type definition.
+    const authenticatedUserId = (req as any).user?.id; 
     
     if (!authenticatedUserId) {
         return res.status(401).json({ message: 'Unauthorized: Authentication required' });
